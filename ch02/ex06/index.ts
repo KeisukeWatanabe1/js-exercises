4ec83a5bec4cb5b78ea7d39945fdf16edd7fb3c0
Watanabe Keisuke 1 (渡邉 圭祐) <keisuke.watanabe1@jp.ricoh.com>
2026-08-29 16:52:48 +0900

    ch02/ex03にフォーマッタを適用

---
+++ ch02/ex06/index.ts
@@ -0,0 +1,1 @@
+export function fizzbuzz() { let str = ""; for (let i = 1; i <= 100; i++) { str += (i % 3 === 0 && i % 5 === 0) ? "FizzBuzz\n" : (i % 3 === 0 ? "Fizz\n" : (i % 5 === 0 ? "Buzz\n" : `${i}\n`)); } return str; }
