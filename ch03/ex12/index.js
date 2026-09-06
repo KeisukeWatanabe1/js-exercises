e62dc09dd1c171d867b7f81f9a308e17feb19766
Watanabe Keisuke 1 (渡邉 圭祐) <Keisuke.Watanabe1@jp.ricoh.com>
2026-09-06 19:43:01 +0900

    ch03/ex12を追加

---
+++ ch03/ex12/index.js
@@ -0,0 +1,14 @@
+class Example {
+    valueOf() {
+        return 1;
+    }
+
+    toString() {
+        return "example"
+    }
+}
+
+let obj = new Example();
+
+console.log(obj - 0); // 数値演算と解釈させ、valueOfが呼ばれるようにする
+console.log(`${obj}`); // テンプレートリテラルでtoStringが呼ばれるようにする
